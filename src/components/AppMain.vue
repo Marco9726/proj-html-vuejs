3388a4428f1866ca03e141f7701a77b6eef9cac8
Marco 9726 <marcosarra9@gmail.com>
2023-01-25 15:00:22 +0100

    aggiunta section con iframe del video yt + slider verticale movies

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,15 +3,177 @@
 	export default {
 		components:{
 			AppJumbotron
+		},
+		data(){
+			return{
+				pathBase: '../../public/img/',
+				newMovies: [
+					{
+						title: 'Quisque auctor Movie in Strack',
+						finalPath: '2-300x300.jpg',
+						views: 953,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'New Movie Quisque in Strack',
+						finalPath: '5-300x300.jpg',
+						views: 722,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'Movie Action Garila English',
+						finalPath: '3-300x300.jpg',
+						views: 364,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'Action Garila English Movie',
+						finalPath: '3-1-300x300.jpg',
+						views: 296,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'Donec English Action movie',
+						finalPath: '2-1-300x300.jpg',
+						views: 481,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'English Action movie in Strack',
+						finalPath: '4-1-300x300.jpg',
+						views: 277,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'Movie Auctor Quisque in Strack',
+						finalPath: '6-300x300.jpg',
+						views: 300,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'auctor Movie Quisque in Strack',
+						finalPath: '5-300x300.jpg',
+						views: 343,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'Donec Movie Quisque in Strack',
+						finalPath: '2-300x300.jpg',
+						views: 361,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					},
+					{
+						title: 'New Movie Quisque in Strack',
+						finalPath: '1-2-300x300.jpg',
+						views: 503,
+						date: 'Febrary 12, 2016',
+						time: '2.30.00'
+					}
+				]
+			}
 		}
 	}
 </script>
 
 <template>
-	<AppJumbotron />
+	<main>
+		<AppJumbotron />
+		<section>
+			<div class="my-container">
+				<div class="section-header">
+					<h3>Movie Playlist</h3>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesettin</p>
+				</div>
+				<div class="my-row-between">
+					<!-- video youtube  -->
+					<div id="yt-container" class="col-8 pe-4">
+						<iframe class="w-100" height="545" src="https://www.youtube.com/embed/BP0k4SdquG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					</div>
+					<!-- lista movies con slider verticale  -->
+					<div id="movie-list" class="col-4 ps-3">
+						<div id="movie-list-header" class="py-3">
+							<h5>New Movie</h5>
+							<span>Plying 24</span>
+						</div>
+						<div id="movie-list-main" class="pb-5">
+							<ul class="list-column">
+								<!-- ciclo gli oggetti movies dell'array newMovies  -->
+								<li v-for="(item,index) in newMovies" :key="index" class="mt-4">
+									<div class="my-row-between">
+										<div class="movie-cover img-fluid">
+											<img :src="pathBase + item.finalPath" :alt="item.title" class="img-fluid">
+										</div>
+										<div class="movie-info ps-3">
+											<h5>{{ item.title }}</h5>
+											<h6>{{ item.views }} views</h6>
+											<div class="my-row-between">
+												<span>{{ item.date }}</span>
+												<span>{{ item.time }}</span>
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+							
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	@use '../scss/generals.scss' as *;
+	@use '../scss/partials/variables' as *;
+
+	section{
+		.section-header{
+			p{
+				font-size: 16px;
+				font-weight: 300;
+				margin-bottom: 0;
+			}
+		}
+		#movie-list{
+			#movie-list-header,
+			#movie-list-main{
+				background-color: $secondary;
+				padding-left: 1.25rem;
+				padding-right: 1.25rem;
+			}
+			#movie-list-main{
+				margin-top: 1px;
+				max-height: 480px;
+				overflow-y: scroll;
+
+				ul{
+					li{
+						.movie-cover{
+							width: 30%;
+						}
+						.movie-info{
+							h6{
+								font-size: 13px;
+							}
+						}
+					}
+				}
+			}
+			
+		}
+		
+	}
+
 	
 </style>
