2736ddc29a68c5777a0636315724548b8829d7ea
Marco 9726 <marcosarra9@gmail.com>
2023-06-23 16:29:18 +0200

    fix flex card movie list

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -1,59 +1,68 @@
 <script>
-	export default {
-		props:{
-			card: Object
-		},
-		data(){
-			return{
-				pathBase: "../../public/img/"
-			}
-		}
-	}
+export default {
+   props: {
+      card: Object,
+   },
+   data() {
+      return {
+         pathBase: '../../public/img/',
+      };
+   },
+};
 </script>
 
 <template>
-	<div class="my-card movie-card">
-		<img :src="pathBase + card.finalPath" :alt="card.title" class="w-100 h-100">
-		<div class="rating">
-			<a href="#"><i class="fa-solid fa-star color-green me-2"></i></a><span>{{ card.stars }}/10</span>
-		</div>
-		<div class="card-title">
-			<h3>{{ card.title }}</h3>
-			<h5 class="me-2">Category:</h5> <h6>{{ card.category }}</h6>
-		</div>
-		<div class="label bottom-label left-label">
-			<h6 class="m-0">Details</h6>
-		</div>
-		<div class="label bottom-label right-label">
-			<h6 class="m-0">{{ card.views }} Views</h6>
-		</div>
-		
-	</div>
+   <div class="my-card movie-card">
+      <img
+         :src="pathBase + card.finalPath"
+         :alt="card.title"
+         class="w-100 h-100"
+      />
+      <div class="rating">
+         <a href="#"><i class="fa-solid fa-star color-green me-2"></i></a
+         ><span>{{ card.stars }}/10</span>
+      </div>
+      <div class="card-title">
+         <h3>{{ card.title }}</h3>
+         <h5 class="me-2">Category:</h5>
+         <h6>{{ card.category }}</h6>
+      </div>
+      <div class="label bottom-label left-label">
+         <h6 class="m-0">Details</h6>
+      </div>
+      <div class="label bottom-label right-label">
+         <h6 class="m-0">{{ card.views }} Views</h6>
+      </div>
+   </div>
 </template>
 
 <style lang="scss">
-	@use '../scss/generals.scss' as *;
+@use '../scss/generals.scss' as *;
 
-	.movie-card{
-		width: calc(100% / 3 - 1.5rem);
-		margin-bottom: 2.25rem;
-		.rating{
-			top: 2.5rem;
-			right: 1.25rem;
-			.fa-star{
-				position: relative;
-				bottom: 1px;
-				font-size: .8rem;
-			}
-		}
-		.card-title{
-			left: 2rem;
-			bottom: 90px;
-			h5,
-			h6{
-				display: inline-block;
-			}
-		}
-	}
-	
+.movie-card {
+   width: calc(100% / 3 - 1.5rem);
+   margin-bottom: 2.25rem;
+
+   &:not(:nth-of-type(3n + 0)) {
+      background: red;
+      margin-right: 2.25rem;
+   }
+   .rating {
+      top: 2.5rem;
+      right: 1.25rem;
+      .fa-star {
+         position: relative;
+         bottom: 1px;
+         font-size: 0.8rem;
+      }
+   }
+   .card-title {
+      left: 2rem;
+      bottom: 90px;
+      h5,
+      h6 {
+         display: inline-block;
+      }
+   }
+}
 </style>
--- src/components/CardMovieList.vue
+++ src/components/CardMovieList.vue
@@ -1,105 +1,105 @@
 <script>
-import CardMovie from "./CardMovie.vue";
+import CardMovie from './CardMovie.vue';
 export default {
-  props: {
-    categorySelected: String,
-  },
-  components: {
-    CardMovie,
-  },
-  data() {
-    return {
-      movieCardsArray: [
-        {
-          title: "Hurry Animate Blue Strack New Movie",
-          category: "Top Rating",
-          finalPath: "1.jpg",
-          views: 381,
-          stars: 0,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-        {
-          title: "Animate Blue Strack New Movie",
-          category: "Latest Movie",
-          finalPath: "5-1.jpg",
-          views: 344,
-          stars: 0,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-        {
-          title: "Blue Strack New Animate Movie",
-          category: "Coming Soon",
-          finalPath: "3.jpg",
-          views: 420,
-          stars: 0,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-        {
-          title: "Nulla Strack New Animate Movie",
-          category: "TV Series",
-          finalPath: "4.jpg",
-          views: 325,
-          stars: 0,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-        {
-          title: "Quisque In Strack New Movie",
-          category: "Top Rating",
-          finalPath: "3-222x300.jpg",
-          views: 340,
-          stars: 1,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-        {
-          title: "New Movie Quisque In Strack",
-          category: "Latest Movie",
-          finalPath: "2.jpg",
-          views: 503,
-          stars: 0,
-          time: "2.30.00",
-          release: "February 12, 2016",
-          genres: "Drama, Action Releas",
-        },
-      ],
-    };
-  },
-  computed: {
-    // metodo per filtrare l'array in base alla categoria selezionata
-    availableMovies() {
-      // se non è vera la categoria o se è uguale a 'All', restituisco l'array originale
-      if (!this.categorySelected || this.categorySelected === "All") {
-        return this.movieCardsArray;
-      }
-      // altrimenti filtro l'array solo con gli oggetti che hanno la categoria uguale a quella selezionata
-      return this.movieCardsArray.filter(
-        (item) => item.category === this.categorySelected
-      );
-    },
-  },
+   props: {
+      categorySelected: String,
+   },
+   components: {
+      CardMovie,
+   },
+   data() {
+      return {
+         movieCardsArray: [
+            {
+               title: 'Hurry Animate Blue Strack New Movie',
+               category: 'Top Rating',
+               finalPath: '1.jpg',
+               views: 381,
+               stars: 0,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+            {
+               title: 'Animate Blue Strack New Movie',
+               category: 'Latest Movie',
+               finalPath: '5-1.jpg',
+               views: 344,
+               stars: 0,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+            {
+               title: 'Blue Strack New Animate Movie',
+               category: 'Coming Soon',
+               finalPath: '3.jpg',
+               views: 420,
+               stars: 0,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+            {
+               title: 'Nulla Strack New Animate Movie',
+               category: 'TV Series',
+               finalPath: '4.jpg',
+               views: 325,
+               stars: 0,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+            {
+               title: 'Quisque In Strack New Movie',
+               category: 'Top Rating',
+               finalPath: '3-222x300.jpg',
+               views: 340,
+               stars: 1,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+            {
+               title: 'New Movie Quisque In Strack',
+               category: 'Latest Movie',
+               finalPath: '2.jpg',
+               views: 503,
+               stars: 0,
+               time: '2.30.00',
+               release: 'February 12, 2016',
+               genres: 'Drama, Action Releas',
+            },
+         ],
+      };
+   },
+   computed: {
+      // metodo per filtrare l'array in base alla categoria selezionata
+      availableMovies() {
+         // se non è vera la categoria o se è uguale a 'All', restituisco l'array originale
+         if (!this.categorySelected || this.categorySelected === 'All') {
+            return this.movieCardsArray;
+         }
+         // altrimenti filtro l'array solo con gli oggetti che hanno la categoria uguale a quella selezionata
+         return this.movieCardsArray.filter(
+            (item) => item.category === this.categorySelected
+         );
+      },
+   },
 };
 </script>
 
 <template>
-  <div class="my-row-between flex-wrap">
-    <!-- ciclo gli oggetti dell'array della funziona avaibleMovies e passo l'oggetto (card) come prop alla componente CardMovie  -->
-    <CardMovie
-      v-for="(item, index) in availableMovies"
-      :key="index"
-      :card="item"
-    />
-  </div>
+   <div class="d-flex flex-wrap">
+      <!-- ciclo gli oggetti dell'array della funziona avaibleMovies e passo l'oggetto (card) come prop alla componente CardMovie  -->
+      <CardMovie
+         v-for="(item, index) in availableMovies"
+         :key="index"
+         :card="item"
+      />
+   </div>
 </template>
 
 <style lang="scss">
-@use "../scss/generals.scss" as *;
+@use '../scss/generals.scss' as *;
 </style>
